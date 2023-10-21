083ebd33131ad7970084f275c0fa1600a96e84bc
gavin <gavin@y62347616.com>
2023-10-21 09:54:02 +0800

    contrasstive_baseline

--- train_retrieval.py
+++ train_retrieval.py
@@ -78,27 +78,27 @@ def evaluation(model, data_loader, device, config):
     print('Computing features for evaluation...')
     start_time = time.time()  
 
-    texts = data_loader.dataset.text   
+    texts = data_loader.dataset.text
     num_text = len(texts)
     text_bs = 256
-    text_ids = []
+    # text_ids = []
     text_embeds = []  
-    text_atts = []
+    # text_atts = []
     for i in range(0, num_text, text_bs):
         text = texts[i: min(num_text, i+text_bs)]
         text_input = model.tokenizer(text, padding='max_length', truncation=True, max_length=35, return_tensors="pt").to(device) 
         text_output = model.text_encoder(text_input.input_ids, attention_mask = text_input.attention_mask, mode='text')  
         text_embed = F.normalize(model.text_proj(text_output.last_hidden_state[:,0,:]))
         text_embeds.append(text_embed)   
-        text_ids.append(text_input.input_ids)
-        text_atts.append(text_input.attention_mask)
+        # text_ids.append(text_input.input_ids)
+        # text_atts.append(text_input.attention_mask)
     
     text_embeds = torch.cat(text_embeds,dim=0)
-    text_ids = torch.cat(text_ids,dim=0)
-    text_atts = torch.cat(text_atts,dim=0)
-    text_ids[:,0] = model.tokenizer.enc_token_id
+    # text_ids = torch.cat(text_ids,dim=0)
+    # text_atts = torch.cat(text_atts,dim=0)
+    # text_ids[:,0] = model.tokenizer.enc_token_id
     
-    image_feats = []
+    # image_feats = []
     image_embeds = []
     for image, img_id in data_loader: 
         image = image.to(device) 
@@ -106,55 +106,56 @@ def evaluation(model, data_loader, device, config):
         image_embed = model.vision_proj(image_feat[:,0,:])            
         image_embed = F.normalize(image_embed,dim=-1)      
         
-        image_feats.append(image_feat.cpu())
+        # image_feats.append(image_feat.cpu())
         image_embeds.append(image_embed)
      
-    image_feats = torch.cat(image_feats,dim=0)
+    # image_feats = torch.cat(image_feats,dim=0)
     image_embeds = torch.cat(image_embeds,dim=0)
     
     sims_matrix = image_embeds @ text_embeds.t()
-    score_matrix_i2t = torch.full((len(data_loader.dataset.image),len(texts)),-100.0).to(device)
+    score_matrix_i2t = sims_matrix
+    # score_matrix_i2t = torch.full((len(data_loader.dataset.image),len(texts)),-100.0).to(device)
     
-    num_tasks = utils.get_world_size()
-    rank = utils.get_rank() 
-    step = sims_matrix.size(0)//num_tasks + 1
-    start = rank*step
-    end = min(sims_matrix.size(0),start+step)
+    # num_tasks = utils.get_world_size()
+    # rank = utils.get_rank() 
+    # step = sims_matrix.size(0)//num_tasks + 1
+    # start = rank*step
+    # end = min(sims_matrix.size(0),start+step)
 
-    for i,sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)): 
-        topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
+    # for i,sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)): 
+    #     topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
 
-        encoder_output = image_feats[start+i].repeat(config['k_test'],1,1).to(device)
-        encoder_att = torch.ones(encoder_output.size()[:-1],dtype=torch.long).to(device)
-        output = model.text_encoder(text_ids[topk_idx], 
-                                    attention_mask = text_atts[topk_idx],
-                                    encoder_hidden_states = encoder_output,
-                                    encoder_attention_mask = encoder_att,                             
-                                    return_dict = True,
-                                   )
-        score = model.itm_head(output.last_hidden_state[:,0,:])[:,1]
-        score_matrix_i2t[start+i,topk_idx] = score + topk_sim
+    #     encoder_output = image_feats[start+i].repeat(config['k_test'],1,1).to(device)
+    #     encoder_att = torch.ones(encoder_output.size()[:-1],dtype=torch.long).to(device)
+    #     output = model.text_encoder(text_ids[topk_idx], 
+    #                                 attention_mask = text_atts[topk_idx],
+    #                                 encoder_hidden_states = encoder_output,
+    #                                 encoder_attention_mask = encoder_att,                             
+    #                                 return_dict = True,
+    #                                )
+    #     score = model.itm_head(output.last_hidden_state[:,0,:])[:,1]
+    #     score_matrix_i2t[start+i,topk_idx] = score + topk_sim
         
-    sims_matrix = sims_matrix.t()
-    score_matrix_t2i = torch.full((len(texts),len(data_loader.dataset.image)),-100.0).to(device)
+    score_matrix_t2i = sims_matrix.t()
+    # score_matrix_t2i = torch.full((len(texts),len(data_loader.dataset.image)),-100.0).to(device)
     
-    step = sims_matrix.size(0)//num_tasks + 1
-    start = rank*step
-    end = min(sims_matrix.size(0),start+step)    
+    # step = sims_matrix.size(0)//num_tasks + 1
+    # start = rank*step
+    # end = min(sims_matrix.size(0),start+step)    
     
-    for i,sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)): 
+    # for i,sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)): 
         
-        topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
-        encoder_output = image_feats[topk_idx].to(device)
-        encoder_att = torch.ones(encoder_output.size()[:-1],dtype=torch.long).to(device)
-        output = model.text_encoder(text_ids[start+i].repeat(config['k_test'],1), 
-                                    attention_mask = text_atts[start+i].repeat(config['k_test'],1),
-                                    encoder_hidden_states = encoder_output,
-                                    encoder_attention_mask = encoder_att,                             
-                                    return_dict = True,
-                                   )
-        score = model.itm_head(output.last_hidden_state[:,0,:])[:,1]
-        score_matrix_t2i[start+i,topk_idx] = score + topk_sim
+    #     topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
+    #     encoder_output = image_feats[topk_idx].to(device)
+    #     encoder_att = torch.ones(encoder_output.size()[:-1],dtype=torch.long).to(device)
+    #     output = model.text_encoder(text_ids[start+i].repeat(config['k_test'],1), 
+    #                                 attention_mask = text_atts[start+i].repeat(config['k_test'],1),
+    #                                 encoder_hidden_states = encoder_output,
+    #                                 encoder_attention_mask = encoder_att,                             
+    #                                 return_dict = True,
+    #                                )
+    #     score = model.itm_head(output.last_hidden_state[:,0,:])[:,1]
+    #     score_matrix_t2i[start+i,topk_idx] = score + topk_sim
 
     if args.distributed:
         dist.barrier()   
